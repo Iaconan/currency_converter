51d0e5f82efcf94d2a3deb2a300e96fa2bdb5cd9
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-05-02 09:27:05 +0200

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+const FIXER_API_KEY = 'OdcMw0NWSdlTQyXriAZ3oeK5XtgrACJO';
+const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
+
+const REST_COUNTRIES_API = `https:://restcountries.eu/rest/v2/currency`;
+
+// 
